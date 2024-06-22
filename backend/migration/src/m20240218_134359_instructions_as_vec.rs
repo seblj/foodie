78046bb5f610b7d3f5e4298daa58d10849f481ad
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-06-22 20:24:44 +0200

    fix warning in migration

--- backend/migration/src/m20240218_134359_instructions_as_vec.rs
+++ backend/migration/src/m20240218_134359_instructions_as_vec.rs
@@ -36,7 +36,7 @@ impl MigrationTrait for Migration {
             .await
     }
 
-    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+    async fn down(&self, _: &SchemaManager) -> Result<(), DbErr> {
         todo!();
     }
 }
